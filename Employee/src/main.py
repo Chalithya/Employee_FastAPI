89401ff36b19dca3d5da2ab6258ac555dbcdf53c
Chalithya Sangeeth <chalithyaofficial@gmail.com>
2021-09-13 14:43:09 +0530

    Basic View and Insert are completed

---
+++ Employee/src/main.py
@@ -0,0 +1,172 @@
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import services, models, schemas
+from .database import SessionLocal, engine
+
+
+
+models.Base.metadata.create_all(bind = engine)
+
+app = FastAPI()
+
+
+#Dependacy creation
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+"""---------------------------------------------------------------------------------------------------------------------------------------
+                                                            Employee
+---------------------------------------------------------------------------------------------------------------------------------------"""
+"""
+        Create
+"""
+
+@app.post("/employees/", response_model = schemas.Employee, tags=["Employee"])
+def create_employee(employee: schemas.EmployeeCreate,  db: Session = Depends(get_db)):
+    db_employee = services.get_employee_by_id(db, id = employee.id)
+    if db_employee:
+        raise HTTPException(status_code = 400, detail = "Employee Id Already Registered")
+    
+    return services.create_employee(db = db, employee = employee) 
+
+
+
+"""
+        View
+"""
+#------------ Read all employees-----------------------------------------------------------------------
+@app.get("/employees/", response_model = List[schemas.Employee], tags=["Employee"])
+def read_employees(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    employees = services.get_employees(db = db, skip = skip, limit = limit)
+    return employees
+
+
+
+#------------ Read by id ------------------------------------------------------------------------------
+@app.get("/employees/{id}", response_model=schemas.Employee, tags=["Employee"])
+def read_employee_by_id(id: str, db: Session = Depends(get_db)):
+    db_employee = services.get_employee_by_id(db, id = id)
+    if db_employee is None:
+        raise HTTPException(status_code=404, detail="Employee ID Not Found")
+    return db_employee
+
+
+
+#------------ Read by Name -----------------------------------------------------------------------------
+@app.get("/employees/name/{name}", response_model = List[schemas.Employee], tags=["Employee"])
+def read_employee_by_name(name: str, db: Session = Depends(get_db)):
+    db_employee = services.get_employee_by_name(db, name = name)
+    if db_employee == []:
+        raise HTTPException(status_code=404, detail="Employee Name Not Found")
+    return db_employee
+
+
+"""
+        Delete
+"""
+# @app.delete("/employees/{id}", response_model=schemas.Employee, tags=["Employee"])
+# def delete_employee(id: str, db: Session = Depends(get_db)):
+#     # db_employee = services.get_employee_by_id(db, id = id)
+#     # if db_employee is None:
+#     #     raise HTTPException(status_code=404, detail="Employee ID Not Found")
+#     # return db_employee
+#     query = models.Employee.delete().where()
+
+# @app.delete("/employees/{id}", response_model=schemas.Employee, tags=["Employee"])
+# def delete_employee(id: str, db: Session = Depends(get_db)):
+#     deleted = services.delete_employee(db, id)
+#     return {"employee_deleted": deleted}
+
+"""---------------------------------------------------------------------------------------------------------------------------------------
+                                                             Project 
+---------------------------------------------------------------------------------------------------------------------------------------"""
+"""
+        Create
+"""
+@app.post("/projects/", response_model = schemas.Project, tags=["Projects"])
+def create_project(project: schemas.ProjectCreate,  db: Session = Depends(get_db)):
+    db_project = services.get_project_by_name(db, pro_name = project.pro_name)
+    if db_project:
+        raise HTTPException(status_code = 400, detail = "Project ID already registered")
+    
+    return services.create_project(db = db, project = project) 
+
+
+
+"""
+        View
+"""
+#------------ Read by id ----------------------------------------------------------------------------------
+@app.get("/projects/{pro_id}", response_model=schemas.Project, tags=["Projects"])
+def read_project_by_id(pro_id: str, db: Session = Depends(get_db)):
+    db_project = services.get_project_by_id(db, pro_id = pro_id)
+    if db_project is None:
+        raise HTTPException(status_code=404, detail="Project Not Found")
+    return db_project
+
+
+
+#------------ Read by Name --------------------------------------------------------------------------------
+@app.get("/projects/name/{pro_name}", response_model=schemas.Project, tags=["Projects"])
+def read_project_by_name(pro_name: str, db: Session = Depends(get_db)):
+    db_project = services.get_project_by_name(db, pro_name = pro_name)
+    if db_project is None:
+        raise HTTPException(status_code=404, detail="Project Not Found")
+    return db_project
+
+
+
+#------------ Read All Projects -----------------------------------------------------------------------------
+@app.get("/projects/", response_model = List[schemas.Project], tags=["Projects"])
+def read_projects(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    projects = services.get_projects(db = db, skip = skip, limit = limit)
+    return projects
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------------------------
+                                                             Allocation 
+---------------------------------------------------------------------------------------------------------------------------------------"""
+"""
+        Create
+"""
+@app.post("/allocation/", response_model = schemas.Allocation, tags=["Allocation"])
+def create_allocation(allocation: schemas.AllocationCreate,  db: Session = Depends(get_db)):
+    
+    return services.create_allocation(db = db, allocation = allocation) 
+
+
+
+"""
+        View
+"""
+#------------ Read All Allocation -----------------------------------------------------------------------------
+@app.get("/allocation/", response_model = List[schemas.Allocation], tags=["Allocation"])
+def read_allocation(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    allocation = services.get_allocations(db = db, skip = skip, limit = limit)
+    return allocation
+
+
+
+#------------ Read by pro_id ---------------------------------------------------------------------------------
+@app.get("/allocation/{pro_id}", response_model = List[schemas.Allocation], tags=["Allocation"])
+def read_allocation_by_pro_id(pro_id: str, db: Session = Depends(get_db)):
+    db_allocation = services.get_allocations_by_pro_id(db, pro_id = pro_id)
+    if db_allocation == []:
+        raise HTTPException(status_code=404, detail="Projects Not Found")
+    return db_allocation
+
+
+
+#------------ Read by emp_id ---------------------------------------------------------------------------------
+@app.get("/allocation/emp_id/{emp_id}", response_model = List[schemas.Allocation], tags=["Allocation"])
+def read_allocation_by_emp_id(emp_id: str, db: Session = Depends(get_db)):
+    db_allocation = services.get_allocations_by_emp_id(db, emp_id = emp_id)
+    if db_allocation == []:
+        raise HTTPException(status_code=404, detail="Projects Not Found")
+    return db_allocation
---
+++ Employee/src/services.py
@@ -0,0 +1,147 @@
+
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import asc, or_
+from sqlalchemy.sql.functions import mode
+from . import models, schemas
+
+
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------
+                                        Employees
+-----------------------------------------------------------------------------------------------------------------------------------------------------"""
+
+"""
+        Read
+"""
+#------------ Read by emp_id------------------------------------------------------------------------------------------
+def  get_employee(db: Session, emp_id: int):
+    return db.query(models.Employee).filter(models.Employee.emp_id == emp_id).first()
+
+
+
+#------------ Read by id-----------------------------------------------------------------------------------------------
+def get_employee_by_id(db: Session, id: str):
+    return db.query(models.Employee).filter(models.Employee.id == id).first()
+
+
+
+#------------ Read by name---------------------------------------------------------------------------------------------
+def get_employee_by_name(db: Session, name: str):
+    return db.query(models.Employee).filter(or_(models.Employee.f_name == name, models.Employee.l_name == name)).all()
+
+
+
+#------------ Read all employees---------------------------------------------------------------------------------------
+def get_employees(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Employee).order_by(asc(models.Employee.emp_id)).offset(skip).limit(limit).all()
+ 
+
+
+"""
+        Create 
+"""
+def create_employee(db: Session, employee: schemas.EmployeeCreate):
+    db_employee = models.Employee(
+        id = employee.id,
+        f_name = employee.f_name,
+        l_name = employee.l_name,
+        nic = employee.nic,
+        phone = employee.phone
+    )
+    db.add(db_employee)
+    db.commit()
+    db.refresh(db_employee)
+    return db_employee
+
+
+"""
+        Delete 
+"""
+# def delete_employee(db: Session, id: str):
+#     # db_employee = db.query(models.Employee).filter(models.Employee.id == id).first()
+#     # db.delete(db_employee)
+#     # db.commit()
+#     # db.refresh(db_employee)
+#     # return db_employee
+#     employee = models.Employee.delete().where(models.Employee.id == id)
+#     print(employee)
+#     results = db.execute(employee)
+#     db.commit()
+ 
+#     # return True
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------
+                                        Project
+-----------------------------------------------------------------------------------------------------------------------------------------------------"""
+  
+"""
+        Read 
+"""
+#------------ Read by pro_id-------------------------------------------------------------------------------------
+def get_project_by_id(db: Session, pro_id: int ):
+    return db.query(models.Project).filter(models.Project.pro_id == pro_id).first()
+
+
+
+#------------ Read by pro_name-----------------------------------------------------------------------------------
+def get_project_by_name(db: Session, pro_name: str ):
+    return db.query(models.Project).filter(models.Project.pro_name == pro_name).first()
+
+
+
+#------------ Read all Projects----------------------------------------------------------------------------------
+def get_projects(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Project).order_by(asc(models.Project.pro_id)).offset(skip).limit(limit).all()
+
+
+
+"""
+        Create 
+"""
+def create_project(db: Session, project: schemas.ProjectCreate):
+    db_project = models.Project(pro_name = project.pro_name)
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+    return db_project
+
+
+
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------
+                                        Allocations
+-----------------------------------------------------------------------------------------------------------------------------------------------------"""
+
+"""
+        Read 
+"""
+#------------ Read by pro_ID------------------------------------------------------------------------------------
+def get_allocations_by_pro_id(db: Session, pro_id: str ):
+    return db.query(models.Allocation).filter(models.Allocation.pro_id == pro_id).all()
+
+
+
+#------------ Read by emp_id------------------------------------------------------------------------------------
+def get_allocations_by_emp_id(db: Session, emp_id: str ):
+    return db.query(models.Allocation).filter(models.Allocation.emp_id == emp_id).all()
+
+
+
+#------------ Read all Projects----------------------------------------------------------------------------------
+def get_allocations(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Allocation).order_by(models.Allocation.id).offset(skip).limit(limit).all()
+
+
+
+"""
+        Create 
+"""
+def create_allocation(db: Session, allocation: schemas.AllocationCreate):
+    db_allocation = models.Allocation(
+        pro_id = allocation.pro_id,
+        emp_id = allocation.emp_id,
+        as_from = allocation.as_from,
+        as_to = allocation.as_to
+        )
+    db.add(db_allocation)
+    db.commit()
+    db.refresh(db_allocation)
+    return db_allocation
+
